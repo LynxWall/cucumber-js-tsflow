fc02209b4934545c02aac39c7dc4e5a8f29ea1cf
Colin Stevens <colinstevens@jhu.edu>
2025-08-12 09:25:52 -0400

    dev-0725 format

--- cucumber-tsflow/src/types/vue-shim.d.ts
+++ cucumber-tsflow/src/types/vue-shim.d.ts
@@ -1,6 +1,6 @@
 // lib/shims/vue-shim.d.ts
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent<Record<string, any>, Record<string, any>, any>;
-  export default component;
+	import type { DefineComponent } from 'vue';
+	const component: DefineComponent<Record<string, any>, Record<string, any>, any>;
+	export default component;
 }

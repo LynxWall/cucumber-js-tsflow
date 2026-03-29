836547edefdc7c85fe8e9cf94c0c50f661457775
Lonnie Wall <lwall8@jhu.edu>
2026-03-28 20:25:37 -0400

    context-refactor Cleanup and updates
    Removed compiled output from src. Added reloadSupport to wrapper.

--- cucumber-tsflow/src/api/wrapper.mjs
+++ cucumber-tsflow/src/api/wrapper.mjs
@@ -3,5 +3,6 @@ import api from './index.js';
 export const convertConfiguration = api.convertConfiguration;
 export const loadConfiguration = api.loadConfiguration;
 export const loadSupport = api.loadSupport;
+export const reloadSupport = api.reloadSupport;
 export const loadSources = api.loadSources;
 export const runCucumber = api.runCucumber;
--- cucumber-tsflow/src/transpilers/esnode.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('ts-node-maintained').register({
-    transpileOnly: true,
-    transpiler: '@lynxwall/cucumber-tsflow/lib/transpilers/esbuild-transpiler'
-});
-require('tsconfig-paths').register();
-//# sourceMappingURL=esnode.js.map
